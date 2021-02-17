57e96bd1144607e15545ecf133f54aa07af2a4a8
Irish <aidanpirish@gmail.com>
2021-02-17 13:33:53 -0500

    reworked labels

--- 2_progress_steps/README.md
+++ 2_progress_steps/README.md
@@ -4,6 +4,8 @@
 
 * Use Web Storage to save progress
 * Reset icon that restarts progress
+* Labels generated dynamically
+* Labels can be toggled between 123 and ABC
 
 ## Future improvements
 
--- 2_progress_steps/script.js
+++ 2_progress_steps/script.js
@@ -1,16 +1,19 @@
 let progress = document.getElementById('progress');
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
-let startOver = document.getElementById('startOver');
+let back = document.getElementById('back');
 let circles = document.querySelectorAll('.circle');
+let radios = document.getElementsByName('label-type');
 
-let currentActive;
+let currentActive, labelType;
 
 try {
     currentActive = localStorage.getItem("currentActive")
+    labelType = localStorage.getItem("labelType")
     update();
 } catch (error) {
     currentActive = 1;
+    labelType = 'numeric';
 }
 
 
@@ -34,7 +37,7 @@ prev.addEventListener('click', () => {
     update()
 });
 
-startOver.addEventListener('click', () => {
+back.addEventListener('click', () => {
     currentActive = 1;
 
     next.disabled = false;
@@ -42,33 +45,61 @@ startOver.addEventListener('click', () => {
     update()
 });
 
+radios.forEach((radio, i) => {
+    radio.addEventListener('click', () => {
+        labelType = radio.value;
+        update()
+    })
+});
+
 function update() {
+
+    radios.forEach((radio, i) => {
+        if (radio.checked || radio.value === labelType) {
+            labelType = radio.value;
+            radio.checked = true;
+        } else {
+            radio.checked = false;
+        }
+    });
+
     circles.forEach((circle, idx) => {
+        let index;
+
+        if(labelType === 'alpha') {
+            index = String.fromCharCode(idx + 65);
+        } else {
+            index = idx + 1;
+        }
+
         if (idx < currentActive) {
             circle.classList.add('active');
         } else {
             circle.classList.remove('active');
         }
+        circle.innerHTML = index;
     });
 
     const actives = document.querySelectorAll('.active');
 
     progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
-    console.log(currentActive, circles.length);
 
     if(currentActive == 1) {
         prev.disabled = true;
         next.disabled = false;
+        back.disabled = true;
     } else if (currentActive == circles.length) {
-        console.log("Hello")
         next.disabled = true;
         prev.disabled = false;
+        back.disabled = false;
     } else {
         prev.disabled = false;
         next.disabled = false;
+        back.disabled = false;
     }
 
     localStorage.setItem("currentActive", currentActive);
+    localStorage.setItem("labelType", labelType);
 
 }
