987fec2c4c5272457f2fd612910cedee4bae3649
Irish <aidanpirish@gmail.com>
2021-02-19 12:10:59 -0500

    directs to wiki

--- 4_hidden_search_widget/script.js
+++ 4_hidden_search_widget/script.js
@@ -7,18 +7,33 @@ let isSearch = false;
 
 btn.addEventListener('click', () => {
     if(isSearch) {
-        fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${input.value}&limit=5`).then(function(resp) {
-        console.log(resp);
+        searchWiki();
+    } else {
+        openSearch();
+     }
+});
+
+function handle(event) {
+    if(event.keyCode === 13) {
+        searchWiki();
+    }
+}
+
+
+
+function openSearch() {
+    search.classList.add('active');
+    isSearch = true;
+    input.focus();
+}
+
+function searchWiki() {
+    fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${input.value}&limit=5`).then(function(resp) {
         return resp.json()
     }).then(function(data) {
-        console.log(data[3][0]);
+        window.location.href = data[3][0];
     })
+}
 
-    } else {
-        search.classList.add('active');
-        isSearch = true;
-        input.focus();
-     }
-   
-});
+
 
