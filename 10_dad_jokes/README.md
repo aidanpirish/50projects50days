0a7609e9ee490860a03b2f64190f5e54de6831ad
Irish <aidanpirish@gmail.com>
2021-03-02 16:42:55 -0500

    added images

--- 10_dad_jokes/README.md
+++ 10_dad_jokes/README.md
@@ -2,8 +2,9 @@
 
 ## Extra credit
 
-* 
+* Added formatting to answer to joke
+* Fetches an image related to the content of the joke
 
 ## Future improvements
 
-* 
+*
--- 10_dad_jokes/script.js
+++ 10_dad_jokes/script.js
@@ -1,11 +1,15 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
+const imageEl = document.getElementById('image')
 
 const config = {
     headers: {
         'Accept': 'application/json',   
     }
 }
+
+const ignore = ["and", "what's", "the", "a", "it", "is", "in", "well", "do", "you", "call", "an", "know", "knows", "someone", "yes", "no", "can", "put", "my", "i", "don't", "think", "they", "their", "there", "they're", "they'll", "me", "your", "i'm", 
+"like", "then", "than", "other", "them", "come", "only", "good", "which", "over", "or", "at", "do", "side", "are", "want", "about", "doesn't", "just", "to", "if", "of", "was", "but", "now", "used", "sure", "not", "few", " few", "read", "what", "i've", "all", "free", "how", "on"]
 
 generateJoke() 
 
@@ -17,6 +21,7 @@ async function generateJoke() {
     const res = await fetch('https://icanhazdadjoke.com/', config)
     const data = await res.json()
 
+    let img = relatedImage(data.joke)
     jokeEl.innerHTML = formatJoke(data.joke)
 }
 
@@ -36,4 +41,59 @@ function formatJoke(joke) {
 
     return joke
 
+}
+
+async function relatedImage(text) {
+    // get each word
+    text = text.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,"").split(" ")
+    console.log(text)
+
+    newText = []
+
+    text.forEach((word, i) => {
+        word = word.toLowerCase()
+        if (!ignore.includes(word)) {
+            newText.push(word)
+        }
+    })
+    console.log(newText)
+    wordsToSearch = getRandom(newText, 2)
+    // console.log(wordToSearch)
+
+    let searchStr = ""
+
+    wordsToSearch.forEach(word => {
+        searchStr = searchStr + word + "+"
+    })
+
+    //build search 
+
+    const res = await fetch(`https://pixabay.com/api/?key=20496271-890413fc848d5b0e8711680d5&q=${searchStr}&image_type=photo`)
+    const data = await res.json()
+
+    console.log(data)
+
+    // let img = relatedImage(data.joke)
+    // jokeEl.innerHTML = formatJoke(data.joke)
+    try {
+        imageEl.src = data.hits[0].previewURL
+    } catch(err) {
+        console.log(err)
+    }
+
+}
+
+
+function getRandom(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
 }
