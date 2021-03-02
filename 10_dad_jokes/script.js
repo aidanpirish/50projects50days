e2ef936a9bb60bcdf95c155b5ee04436ad201682
Irish <aidanpirish@gmail.com>
2021-03-02 11:39:45 -0500

    added formating to answer

--- 10_dad_jokes/script.js
+++ 10_dad_jokes/script.js
@@ -17,5 +17,23 @@ async function generateJoke() {
     const res = await fetch('https://icanhazdadjoke.com/', config)
     const data = await res.json()
 
-    jokeEl.innerHTML = data.joke
+    jokeEl.innerHTML = formatJoke(data.joke)
+}
+
+function formatJoke(joke) {
+    // if text includes '?', highlight the answer
+    if(joke.includes("?")) {
+        let qIndex = joke.indexOf('?')
+        //get answer
+        var answer = joke.slice(qIndex+1)
+        //get joke without answer
+        var joke = joke.slice(0, qIndex+2)
+        //add answer back with span with class answer
+        answer = `<span class="answer">${answer}</span>`
+        joke = joke + answer
+
+    }
+
+    return joke
+
 }
